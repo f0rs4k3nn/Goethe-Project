9e850787d71dfc2d2b7305085b7010d8bd622999
Eduard <danflorineduard@gmail.com>
2020-09-21 16:17:11 +0300

    More models

---
+++ Assets/Scripts/Misc/PickUpText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpText : MonoBehaviour
+{
+    public string pickUpText;
+
+    static bool pickUpKey;
+
+    private void OnTriggerStay(Collider player)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && gameObject.name == "Box")
+        {
+            if (!pickUpKey)
+            {
+                pickUpText = "You found a key!";
+                pickUpKey = true;               
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
