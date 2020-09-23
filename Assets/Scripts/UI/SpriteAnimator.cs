de2ef53b4aafb1051b983a4f89dbf51ff63f90a3
Lunastras <radu_bucurescu@yahoo.com>
2020-09-23 09:27:08 +0100

    Cutscene scene prototype

---
+++ Assets/Scripts/UI/SpriteAnimator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteAnimator : MonoBehaviour
+{
+    public float timeBetweenFrames = 0.1f;
+    public bool randomFrames;
+
+    public Sprite[] sprites;
+
+    private Image _image;
+    private int _currentIndex;
+    private int _length;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _image = GetComponent<Image>();
+        _length = sprites.Length;
+
+        StartCoroutine(StartAnimation());
+    }
+
+    private IEnumerator StartAnimation()
+    {
+        while(true)
+        {
+            if (randomFrames)
+            {
+                _currentIndex = Random.Range(0, _length);
+            }
+            else
+            {
+                _currentIndex++;
+
+                if (_currentIndex == _length)
+                {
+                    _currentIndex = 0;
+                }
+            }
+
+            _image.sprite = sprites[_currentIndex];
+
+            yield return new WaitForSeconds(timeBetweenFrames);
+        }
+    } 
+}
