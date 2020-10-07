4c286b2417d4b43c877d9f6d07701bb6a3632006
cezatul <58083143+cezatul@users.noreply.github.com>
2020-10-07 19:44:00 +0300

    IndoorLvL

---
+++ Assets/LightScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+	    Light myLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         myLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myLight.intensity = Mathf.PingPong(Time.time, 2);
+    }
+}
