fbb965d0e4e9a5b3aa339c790121535f368080ce
bertugg <bertugbenim@gmail.com>
2020-10-10 01:58:26 +0300

    Credits Scene Added Start Credits Updated Fonts Updated

---
+++ Assets/CreditsSceneController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsSceneController : MonoBehaviour
+{
+    [SerializeField]private Animator textAnimator;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            textAnimator.speed = 3;
+        else
+            textAnimator.speed = 1;
+    }
+
+    public void OnAnimationEnd()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
