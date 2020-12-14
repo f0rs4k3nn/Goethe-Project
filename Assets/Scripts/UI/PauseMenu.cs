c706ca0220b595926305e9bff86f407671338484
silviuhogas <72731970+silviuhogas@users.noreply.github.com>
2020-12-14 13:14:38 +0200

    butonPauza

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -10,7 +10,7 @@ public class PauseMenu : MonoBehaviour
     public Image fadeOverlay;
     public float fadeDelay = 1.0f;
     public GameObject pauseMenu;
-
+    public GameObject pauseButton;
     private Toggle m_MenuToggle;
     private float m_TimeScaleRef = 1f;
     private float m_VolumeRef = 1f;
@@ -58,10 +58,10 @@ public class PauseMenu : MonoBehaviour
 
 
 
-    private void MenuOn()
+    public void MenuOn()
     {
-        Cursor.lockState = CursorLockMode.Confined;
-        Cursor.visible = true;
+       // Cursor.lockState = CursorLockMode.Confined;
+       // Cursor.visible = true;
 
         m_TimeScaleRef = Time.timeScale;
         Time.timeScale = 0f;
@@ -70,36 +70,37 @@ public class PauseMenu : MonoBehaviour
         AudioListener.volume = 0f;
 
         m_Paused = true;
-
+        pauseButton.SetActive(false);
         pauseMenu.SetActive(true);
     }
 
 
     public void MenuOff()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+      // Cursor.lockState = CursorLockMode.Locked;
+      // Cursor.visible = false;
 
         pauseMenu.SetActive(false);
         Time.timeScale = m_TimeScaleRef;
         AudioListener.volume = m_VolumeRef;
         m_Paused = false;
+        pauseButton.SetActive(true);
         AudioManager.instance.Play("Selection");
     }
 
 #if !MOBILE_INPUT
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Escape) && !levelFinish.active)
-        {
-            if(m_Paused)
-            {
-                MenuOff();
-            } else
-            {
-                MenuOn();
-            }
-        }
+       if (Input.GetKeyUp(KeyCode.Escape) && !levelFinish.active)
+       {
+           if(m_Paused)
+           {
+               MenuOff();
+           } else
+           {
+               MenuOn();
+           }
+       }
     }
 #endif
 }
