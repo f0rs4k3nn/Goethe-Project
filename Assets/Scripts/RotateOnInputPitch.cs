58950b2c38e05bd71e890b552e8bba21899df135
Popescu Andrei Iulian <andrei.programare@gmail.com>
2021-01-19 14:33:32 +0200

    Added the second Joystick for camera movement

---
+++ Assets/Scripts/RotateOnInputPitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOnInputPitch : MonoBehaviour
+{
+   
+    public FloatingJoystick RotationJoystick;
+    public float speed = 100.0f;
+    
+    private float rotationPitch = 0.0f;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void  Update()
+    {   
+        Transform partent = gameObject.GetComponentInParent<Transform>();
+        rotationPitch += RotationJoystick.Vertical * speed *Time.deltaTime;
+        transform.eulerAngles = new Vector3(rotationPitch,partent.transform.eulerAngles.y,0.0f);
+        
+        //transform.eulerAngles = new Vector3(0.0f,0.0f,rotationPitch);
+    }
+}
