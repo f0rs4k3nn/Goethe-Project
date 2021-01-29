7a925d290065e7ba7bd4e39171072ed05d9c3fdc
chirica-dragos <67731768+chirica-dragos@users.noreply.github.com>
2021-01-29 18:16:11 +0200

    Revert "Revert "Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/f0rs4k3nn/Goethe-Project""""""
    This reverts commit efceafac849850db8fe6ee149bf786b7523b913d.

---
+++ Assets/Scripts/RotateOnInputYaw.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOnInputYaw : MonoBehaviour
+{
+  
+    public FloatingJoystick RotationJoystick;
+    public float speed = 5.0f;
+    private float rotationYaw = 0.0f;
+   
+    void  Update()
+    {
+        rotationYaw += RotationJoystick.Horizontal *speed*Time.deltaTime;
+        transform.eulerAngles = new Vector3(0.0f,rotationYaw,0.0f);
+    }
+}
