b5cead8faf3af09ddb66bca9fb9038dec9c7c5d9
chirica-dragos <67731768+chirica-dragos@users.noreply.github.com>
2021-01-29 13:07:54 +0200

    Revert "Merge branch 'master' of https://github.com/f0rs4k3nn/Goethe-Project"
    This reverts commit dbaa73d7e50d2d7ad404202f63d3dff0d9de431f, reversing
    changes made to 19a2a1498200911d81191a114d14457b28e976e3.

--- Assets/Assets Download/Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
+++ Assets/Assets Download/Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
@@ -1,32 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
-//[System.Serializable]
-
-
-public class FixedJoystick : MonoBehaviour
+public class FixedJoystick : Joystick
 {
-    
-    // public RectTransform Background;
-    // public RectTransform Handle;
-    // public JoyStickDirection JoyStickDirection = JoyStickDirection.Both;
-    // [Randge(0,2.0f)] public float HandleLimit = 1f;
-    // public Vector2 input = Vector2.zero;
-
-
-    // public void OnPointerDown(PointerEventData eventData)
-    // {
-
-    // }
-    // public void OnDrag(PointerEventData eventData)
-    // {
-    //     Vector2 JoyDirection = eventData.position - RectTransformUtility.WorldToScreenPoint(new Camera(),Background.position);
-    //     input = JoyDirection.magnitude>Background.sizeDelta.x/2f)? JoyDirection.normalized : JoyDirection/(Background.sizeDelta.x/2f);
-    //     Handle.anchoredPosition = (input*Background.sizeDelta.x/2f)*HandleLimit;
-    // }
-
 
 }
