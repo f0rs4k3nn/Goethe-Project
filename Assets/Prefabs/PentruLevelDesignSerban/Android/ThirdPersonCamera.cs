a547ec412faa62d7177ec00ce4a5ed0082a2a640
chirica-dragos <67731768+chirica-dragos@users.noreply.github.com>
2021-01-29 18:08:11 +0200

    Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/f0rs4k3nn/Goethe-Project""""
    This reverts commit 8f7f761cba4609cea5722e70d75d2333fb6cd88a.

--- Assets/Prefabs/PentruLevelDesignSerban/Android/ThirdPersonCamera.cs
+++ Assets/Prefabs/PentruLevelDesignSerban/Android/ThirdPersonCamera.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -28,32 +28,34 @@ public class ThirdPersonCamera : MonoBehaviour
 
     void LateUpdate()
     {
-        if (target)
-        {
-            // Calculate the current rotation angles
-            wantedRotationAngleY = target.eulerAngles.y;
-            wantedRotationAngleX = target.eulerAngles.x + rotation;
-            wantedHeight = target.position.y + height;
-            currentRotationAngleY = transform.eulerAngles.y;
-            currentRotationAngleX = transform.eulerAngles.x;
-            currentHeight = transform.position.y;
-            // Damp the rotation around the y-axis
-            currentRotationAngleY = Mathf.LerpAngle(currentRotationAngleY, wantedRotationAngleY, rotationDamping * Time.deltaTime);
-            currentRotationAngleX = Mathf.LerpAngle(currentRotationAngleX, wantedRotationAngleX, rotationDamping * Time.deltaTime);
-            // Damp the height
-            currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+         if (target)
+         {
+          
+             //Calculate the current rotation angles
+             wantedRotationAngleY = target.eulerAngles.y;
+             wantedRotationAngleX = target.eulerAngles.x + rotation;
+             wantedHeight = target.position.y + height;
+             currentRotationAngleY = transform.eulerAngles.y;
+             currentRotationAngleX = transform.eulerAngles.x;
+             currentHeight = transform.position.y;
+
+             // Damp the rotation around the y-axis
+             currentRotationAngleY = Mathf.LerpAngle(currentRotationAngleY, wantedRotationAngleY, rotationDamping * Time.deltaTime);
+             currentRotationAngleX = Mathf.LerpAngle(currentRotationAngleX, wantedRotationAngleX, rotationDamping * Time.deltaTime);
+             // Damp the height
+             currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
             // Convert the angle into a rotation
-            currentRotation = Quaternion.Euler(currentRotationAngleX, currentRotationAngleY, 0f);
-            // Set the position of the camera on the x-z plane to:
-            // distance meters behind the target
-            transform.position = target.position;
-            transform.position -= currentRotation * Vector3.forward * distance;
+             currentRotation = Quaternion.Euler(currentRotationAngleX, currentRotationAngleY, 0f);
+             // Set the position of the camera on the x-z plane to:
+             // distance meters behind the target
+             transform.position = target.position;
+             transform.position -= currentRotation * Vector3.forward * distance;
             // Set the height of the camera
             transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
-            // Always look at the target
-            if (shouldRotate)
-                transform.LookAt(target);
-        }
+             // Always look at the target
+             if (shouldRotate)
+                 transform.LookAt(target);
+         }
     }
 
     public static float shakeMagnitude = 0f;
@@ -65,6 +67,11 @@ public class ThirdPersonCamera : MonoBehaviour
     {
         GameManager.Instance.camera = this;
         shakeMagnitude = 0;
+
+         if (shouldRotate)
+         {
+                transform.LookAt(target);
+            }
     }
 
     void Start()
