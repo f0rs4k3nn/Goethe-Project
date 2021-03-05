69c4ef8d74f4ff77d6a4aa885560d4905b4232a8
Eduard <danflorineduard@gmail.com>
2021-03-05 11:03:57 +0200

    serban prefabs

---
+++ Assets/Prefabs/Serban Prefabs/SetResolution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SetResolution : MonoBehaviour
+{
+    public RenderPipelineAsset[] qualityLevels;
+    public TMP_Dropdown dropdown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropdown.value = QualitySettings.GetQualityLevel();
+    }
+
+    // Update is called once per frame
+    public void ChangeLevel(int value)
+    {
+        QualitySettings.SetQualityLevel(value);
+        QualitySettings.renderPipeline = qualityLevels[value];
+    }
+}
